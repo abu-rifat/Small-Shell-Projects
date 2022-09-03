c290b8764422c8888117f29a5a35f6969df07be3
abu-rifat <rifat.cse4.bu@gmail.com>
2022-09-03 06:10:42 +0600

    number-puzzle complete

--- number-puzzle/main.sh
+++ number-puzzle/main.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-#ban="figlet"
-#sudo dpkg -s $ban &> /dev/null
-#if [ $? -eq 0 ];
-#then
-#    ok=1;
-#else
-#    sudo apt install figlet
-#fi
 
 validUser=false
 curruser=""
@@ -24,23 +16,29 @@ fi
 
 if [ ! -f "leaderboard.score" ]
 then
-    touch leaderboard.score
+    echo "No. Score Username" > leaderboard.score
 fi
 
 gameBanner () {
-    clear
+    tput clear
+    tput setaf 3
     echo ""
-    figlet -f smblock "    8-Puzzle Game"
+    echo "8-Puzzle Game"
     echo ""
+    tput sgr0
 }
 
 mainMenu () {
     gameBanner
-    echo "    N - New User SignUp"
-    echo "    L - Existing User Login"
-    echo "    E - Exit"
+    tput rev
+    echo "Signup / Login:"
     echo ""
-    read -p "    Enter your choice: " option
+    tput sgr0
+    echo "N - New User SignUp"
+    echo "L - Existing User Login"
+    echo "E - Exit"
+    echo ""
+    read -p "Enter your choice: " option
     case $option in
         N | n)
             signUp
@@ -49,25 +47,175 @@ mainMenu () {
             login
         ;;
         E | e)
+        clear
         exit
         ;;
         *)
-        echo "Invalid Choice, Press Anykey to Continue..\n"
-        read hold
+        read -p "Invalid Choice, Press Anykey to Continue.. " hold
         ;; 
     esac
 }
 
+signUp () {
+    gameBanner
+    tput rev
+    echo "Signup:"
+    echo ""
+    tput sgr0
+    while : ; do
+        read -p "Enter username(Only lowercase English letters): " username
+        dir="./userpass/$username.usr"
+        scho="./scores/$username.sco"
+        REGEX='^[a-z]+$'
+        if [[ ! $username =~ $REGEX ]]
+        then
+            read -p "Invalid username, want to try again? [Y/N]: " tryagain
+            case $tryagain in
+            N | n)
+                return
+                ;;
+            esac
+        elif [[ -f "$dir" ]]
+        then
+            read -p "The username: $username is taken, want to try again? [Y/N]: " tryagain
+            case $tryagain in
+            N | n)
+                return
+                ;;
+            esac
+        else
+            break
+        fi
+    done
+
+    while : ; do
+        stty -echo
+        read -p "Enter new password(length: 6-10):" password
+        stty echo
+        echo ""
+        REGEX='^.{6,10}$'
+        if [[ ! $password =~ $REGEX ]]
+        then
+            read -p "Password length must be between 6 to 10 characters, want to try again? [Y/N]: " tryagain
+            case $tryagain in
+            N | n)
+                return
+                ;;
+            esac
+        else
+            break
+        fi
+    done
+    
+    while : ; do
+        stty -echo
+        read -p "Enter the password again:" rePassword
+        stty echo
+        echo ""
+        if [[ ! $password == $rePassword ]]
+        then
+            read -p "The two passwords didn't matched, want to try again? [Y/N]: " tryagain
+            case $tryagain in
+            N | n)
+                return
+                ;;
+            esac
+        else
+
+            echo "$(md5sum <<<$password)" > $dir
+            echo "No. Score" > $scho
+            validUser=true
+            curruser=$username
+            read -p "Welcome $username! Press Anykey to Continue.. " hold
+            break;
+        fi
+    done
+}
+
+login () {
+    gameBanner
+    tput rev
+    echo "Login:"
+    echo ""
+    tput sgr0
+    while : ; do
+        read -p "Enter username(Only lowercase English letters): " username
+        dir="./userpass/$username.usr"
+        REGEX='^[a-z]+$'
+        if [[ ! $username =~ $REGEX ]]
+        then
+            read -p "Invalid username, want to try again? [Y/N]: " tryagain
+            case $tryagain in
+            N | n)
+                return
+                ;;
+            esac
+        elif [[ -f "$dir" ]]
+        then
+            while : ; do
+                stty -echo
+                read -p "Enter the password(length: 6-10):" password
+                stty echo
+                echo ""
+                REGEX='^.{6,10}$'
+                if [[ ! $password =~ $REGEX ]]
+                then
+                    read -p "Password length must be between 6 to 10 characters, want to try again? [Y/N]: " tryagain
+                    case $tryagain in
+                    N | n)
+                        return
+                        ;;
+                    esac
+                else
+                    #savedPass=$(head -n 1 $dir)
+                    savedPass=""
+                    while read -r line; do
+                        savedPass=$line
+                        break
+                    done < $dir
+                    passHash=$(md5sum <<<$password)
+                    if [[ $passHash = $savedPass ]]
+                    then
+                        validUser=true
+                        curruser=$username
+                        read -p "Welcome $username! Press Anykey to Continue.. " hold
+                        return;
+                    else
+                        read -p "Wrong password, want to try again? [Y/N]: " tryagain
+                        case $tryagain in
+                        N | n)
+                            return
+                            ;;
+                        esac
+                    fi
+                fi
+            done
+            
+            
+        else
+            read -p "User doesn't exist, want to try again? [Y/N]: " tryagain
+            case $tryagain in
+            N | n)
+                break
+                ;;
+            esac
+        fi
+    done
+}
+
 userMenu () {
     gameBanner
-    echo "    Welcome, $username"
-    echo "    N - Start New Game"
-    echo "    M - My Scoreboard"
-    echo "    L - Leaderboard"
-    echo "    X - Logout"
-    echo "    E - Exit"
+    tput rev
+    echo "Welcome, $username!"
     echo ""
-    read -p "    Enter your choice: " option
+    tput sgr0
+    echo "N - Start New Game"
+    echo "M - My Scoreboard"
+    echo "L - Leaderboard"
+    echo "X - Logout"
+    echo "E - Exit"
+    echo ""
+    read -p "Enter your choice: " option
     case $option in
         N | n)
             newGame
@@ -82,149 +230,44 @@ userMenu () {
             logOut
         ;;
         E | e)
+            clear
             exit
         ;;
         *)
-            echo "Invalid Choice, Press Anykey to Continue..\n"
+            read -p "Invalid Choice, Press Anykey to Continue.. " hold
             read hold
         ;; 
     esac
 }
 
-
-
-
-signUp () {
+myScore () {
     gameBanner
-    echo "    New User SignUp:"
+    dir="./scores/$username.sco"
+    tput rev
+    echo "Your Scoreboard:"
     echo ""
-    while : ; do
-        read -p "    Enter username(Only lowercase English letters): " username
-        dir="./userpass/$username.usr"
-        scho="./scores/$username.usr"
-        REGEX='^[a-z]+$'
-        if [[ ! $username =~ $REGEX ]]
-        then
-            read -p "    Invalid username, want to try again? [Y/N]: " tryagain
-            case $tryagain in
-            N | n)
-                return
-                ;;
-            esac
-        elif [[ -f "$dir" ]]
-        then
-            read -p "    The username: $username is taken, want to try again? [Y/N]: " tryagain
-            case $tryagain in
-            N | n)
-                return
-                ;;
-            esac
-        else
-            break
-        fi
-    done
-
-    while : ; do
-        read -p "    Enter new password(length: 6-10):" password
-        REGEX='^.{6,10}$'
-        if [[ ! $password =~ $REGEX ]]
-        then
-            read -p "    Password length must be between 6 to 10 characters, want to try again? [Y/N]: " tryagain
-            case $tryagain in
-            N | n)
-                return
-                ;;
-            esac
-        else
-            break
-        fi
-    done
-    
-    while : ; do
-        read -p "    Enter the password again:" rePassword
-        if [[ ! $password == $rePassword ]]
-        then
-            read -p "    The two passwords didn't matched, want to try again? [Y/N]: " tryagain
-            case $tryagain in
-            N | n)
-                return
-                ;;
-            esac
-        else
-            echo $password > $dir
-            touch $scho
-            validUser=true
-            curruser=$username
-            read -p "    Welcome $username, press anykey to continue:" hold
-            break;
-        fi
-    done
+    tput sgr0
+    column $dir -tc2
+    echo ""
+    read -p "Press Anykey to Continue.. " hold
 }
 
-login () {
+leaderBoard () {
     gameBanner
-    echo "    User Login:"
+    dir="./leaderboard.score"
+    tput rev
+    echo "Leaderboard:"
     echo ""
-    while : ; do
-        read -p "    Enter username(Only lowercase English letters): " username
-        dir="./userpass/$username.usr"
-        REGEX='^[a-z]+$'
-        if [[ ! $username =~ $REGEX ]]
-        then
-            read -p "    Invalid username, want to try again? [Y/N]: " tryagain
-            case $tryagain in
-            N | n)
-                return
-                ;;
-            esac
-        elif [[ -f "$dir" ]]
-        then
-            while : ; do
-                read -p "    Enter the password(length: 6-10):" password
-                REGEX='^.{6,10}$'
-                if [[ ! $password =~ $REGEX ]]
-                then
-                    read -p "    Password length must be between 6 to 10 characters, want to try again? [Y/N]: " tryagain
-                    case $tryagain in
-                    N | n)
-                        return
-                        ;;
-                    esac
-                else
-                    savedPass=$(head -n 1 $dir)
-                    if [[ $password = $savedPass ]]
-                    then
-                        validUser=true
-                        curruser=$username
-                        read -p "    Welcome $username, press anykey to continue:" hold
-                        return;
-                    else
-                        read -p "    Wrong password, want to try again? [Y/N]: " tryagain
-                        case $tryagain in
-                        N | n)
-                            return
-                            ;;
-                        esac
-                    fi
-                fi
-            done
-            
-            
-        else
-            read -p "    User doesn't exist, want to try again? [Y/N]: " tryagain
-            case $tryagain in
-            N | n)
-                break
-                ;;
-            esac
-        fi
-    done
+    tput sgr0
+    column $dir -tc2
+    echo ""
+    read -p "Press Anykey to Continue.. " hold
 }
 
 logOut() {
     validUser=false
     curruser=""
-    read -p "    See you soon, press anykey to continue:" hold
+    read -p "See you soon, Press Anykey to Continue.. " hold
     break;
 }
 
@@ -235,30 +278,35 @@ target=( "${board[@]}" )
 empty=8
 last=0
 A=0 B=1 C=2 D=3
+nocursor='\e[?25l'
 
-fmt="$nocursor$topleft
+fmt="$nocursor
      %2s  %2s   %2s
      %2s  %2s   %2s
      %2s  %2s   %2s
 "
 
-fmt="\t┏━━━━┳━━━━┳━━━━┓
-\t┃    ┃    ┃    ┃
-\t┃ %2s ┃ %2s ┃ %2s ┃
-\t┃    ┃    ┃    ┃
-\t┣━━━━╋━━━━╋━━━━┫
-\t┃    ┃    ┃    ┃
-\t┃ %2s ┃ %2s ┃ %2s ┃
-\t┃    ┃    ┃    ┃
-\t┣━━━━╋━━━━╋━━━━┫
-\t┃    ┃    ┃    ┃
-\t┃ %2s ┃ %2s ┃ %2s ┃
-\t┃    ┃    ┃    ┃
-\t┗━━━━┻━━━━┻━━━━┛\n\n"
+fmt="$nocursor┏━━━━┳━━━━┳━━━━┓
+┃    ┃    ┃    ┃
+┃ %2s ┃ %2s ┃ %2s ┃
+┃    ┃    ┃    ┃
+┣━━━━╋━━━━╋━━━━┫
+┃    ┃    ┃    ┃
+┃ %2s ┃ %2s ┃ %2s ┃
+┃    ┃    ┃    ┃
+┣━━━━╋━━━━╋━━━━┫
+┃    ┃    ┃    ┃
+┃ %2s ┃ %2s ┃ %2s ┃
+┃    ┃    ┃    ┃
+┗━━━━┻━━━━┻━━━━┛\n\n"
 
 print_board()
 {
     gameBanner
+    tput rev
+    echo "Play:"
+    echo ""
+    tput sgr0
     printf "$fmt" "${board[@]}"
 }
 
@@ -307,18 +355,13 @@ shuffle()
 }
 
 newGame() {
-    trap 'printf "$normal"' EXIT
-    clear
     print_board
-    echo
-    printf "
-    Use the cursor keys to move the tiles around.
-    The game is finished when you return to the
-    position shown above.
-    Try to complete the puzzle in as few moves
-    as possible.
-            Press \e[1mENTER\e[0m to continue
-    "
+    echo "Game Rules:"
+    echo "1. Use Arrow Keys to Move the Tiles."
+    echo "2. Target of This Game is to Make the Tiles Sorted like Above."
+    echo "3. Try to Minimize the Number of Moves to Solve the Game."
+    echo ""
+    echo "Are you ready? Press Enter to Start the Game... "
     shuffle
     moves=0
     read -s
@@ -332,7 +375,16 @@ newGame() {
         if [ "${board[*]}" = "${target[*]}" ]
         then
             print_board
-            printf "\tCompleted in %d moecho "SignUp Working!"ves\n\n" "$moves"
+            led="./leaderboard.score"
+            dir="./scores/$username.sco"
+            linesled=$(wc -l < $led)
+            echo "$linesled $moves $username" >> $led
+            linesdir=$(wc -l < $dir)
+            echo "$linesdir $moves" >> $dir
+            sortLeaderboard
+            #sortScore
+            printf "Congratulations, Completed in %d moves! Press Anykey to Continue... " "$moves"
+            read hold
             break
         fi
         read -sn1 -p $'        \e[K' key
@@ -345,6 +397,65 @@ newGame() {
             q) echo; break ;;
         esac
     done
+}
+
+sortScore () {
+    if [[ ! -f "./.tmp" ]]
+    then
+        touch .tmp
+    fi
+    if [[ ! -f "./t.mp2" ]]
+    then
+        touch .tmp2
+    fi
+    dir="./scores/$username.sco"
+    tmp2dir="./.tmp2"
+    tmpdir="./.tmp"
+    tail -n +2 $dir > $tmp2dir
+    >$tmpdir
+    while read -r no score; do
+        echo "$score" >> $tmpdir
+    done < $tmp2dir
+    sort -n $tmpdir > $tmp2dir
+    no=1
+    >$dir
+    echo "No. Score" > $dir
+    while read -r score; do
+        echo "$no $score" >> $dir
+        no=$(( $no + 1 ))
+    done < $tmp2dir
+    rm $tmpdir
+    rm $tmp2dir
+}
+
+sortLeaderboard () {
+    if [[ ! -f "./.tmp" ]]
+    then
+        touch .tmp
+    fi
+    if [[ ! -f "./t.mp2" ]]
+    then
+        touch .tmp2
+    fi
+    dir="./leaderboard.score"
+    tmp2dir="./.tmp2"
+    tmpdir="./.tmp"
+    tail -n +2 $dir > $tmp2dir
+    >$tmpdir
+    while read -r no score thisuser; do
+        echo "$score $thisuser" >> $tmpdir
+    done < $tmp2dir
+    #sort -k1,1 -k2,2 $tmpdir > $tmp2dir
+    sort $tmpdir > $tmp2dir
+    no=1
+    >$dir
+    echo "No. Score Username" > $dir
+    while read -r score nowuser; do
+        echo "$no $score $nowuser" >> $dir
+        no=$(( $no + 1 ))
+    done < $tmp2dir
+    rm $tmpdir
+    rm $tmp2dir
 }
 
 #Game Code End
@@ -360,3 +471,5 @@ do
     fi
     
 done
+
+
